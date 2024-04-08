d88b717633aee13a4a0f09b0dc66e2bef2bb534b
ILOtaku <164941151+ILOtaku@users.noreply.github.com>
2024-04-08 18:39:11 +0200

    Added get mail
    added get mail function to retrieve a user from the email in the BDD

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,28 @@ Route::get('/', function () {
 Route::get('/comment_layout', function () {
     return view('comment');
 });
+<<<<<<< Updated upstream
+=======
+Route::get('/test/route/helloword', function () {
+    return view('/layout/app');
+});
+
+Route::get('/Register', function () {
+    return view('/Register');
+});
+
+Route::view('/', 'home')->name('home');
+Route::view('/Ressources', 'Ressources')->name('Ressources');
+Route::view('/Favoris', 'Favoris')->name('Favoris');
+Route::view('/aboutus', 'aboutUs')->name('aboutus');
+
+Route::get('/getMail/{mail}', [LoginController::class, 'getMail']);
+Route::get('/checkLogin/{mail}', [LoginController::class, 'checkLogin']);
+
+Route::post('/postUser', [LoginController::class, 'postUser']);
+
+//Route::get('/getPassword/{pass}', [FrontController::class, 'getPassword']);
+
+
+
+>>>>>>> Stashed changes
